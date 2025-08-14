47836fd32a2b70099a24d29c24d5b284f8e2a005
Lucas Sola <lsolagc@hotmail.com>
2025-08-14 20:03:38 -0300

    feat: add PlayerCharacter model

---
+++ modulo03-fundamentos-avancados-ruby/projeto-final/db/migrate/20250813194830_create_player_characters.rb
@@ -0,0 +1,9 @@
+class CreatePlayerCharacters < ActiveRecord::Migration[8.0]
+  def change
+    create_table :player_characters do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
