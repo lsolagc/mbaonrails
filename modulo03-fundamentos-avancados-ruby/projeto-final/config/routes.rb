7a5801e83d5f071fde6a7de7dcf0178848a00fe4
Lucas Sola <lsolagc@hotmail.com>
2025-08-14 20:10:17 -0300

    feat: add routes for PlayerCharacterController

--- modulo03-fundamentos-avancados-ruby/projeto-final/config/routes.rb
+++ modulo03-fundamentos-avancados-ruby/projeto-final/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "player_characters" => "player_characters#index"
+  get "player_characters/:id" => "player_characters#show"
+  post "player_characters" => "player_characters#create"
+  delete "player_characters/:id" => "player_characters#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
