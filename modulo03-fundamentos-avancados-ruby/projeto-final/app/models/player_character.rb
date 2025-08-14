77f8f85cb93c6b56dba11d7fe8b94aa2c2c71bd8
Lucas Sola <lsolagc@hotmail.com>
2025-08-14 20:11:10 -0300

    feat: add #attack and #take_damage methods

--- modulo03-fundamentos-avancados-ruby/projeto-final/app/models/player_character.rb
+++ modulo03-fundamentos-avancados-ruby/projeto-final/app/models/player_character.rb
@@ -7,4 +7,27 @@ class PlayerCharacter < ApplicationRecord
 
   has_one :combatant, as: :combatable, touch: true, dependent: :destroy
 
+  def attack(target:)
+    raise ArgumentError, "Target must be a Combatant" unless target.respond_to?(:combatant)
+
+    attack_roll = rand(1..20)
+    if attack_roll >= target.armor_class
+      damage = rand(1..4) + strength_modifier
+      target.take_damage(amount: damage)
+      { success: true, attack_roll: attack_roll, damage: damage, message: "Hit!" }
+    else
+      { success: false, attack_roll: attack_roll, message: "Miss!" }
+    end
+  end
+
+  def take_damage(amount:)
+    raise ArgumentError, "Amount must be a positive Integer" unless amount.is_a?(Integer) && amount.positive?
+
+    self.current_hit_points ||= max_hit_points
+    self.current_hit_points -= amount
+
+    if current_hit_points <= 0
+      self.current_hit_points = 0
+    end
+  end
 end
