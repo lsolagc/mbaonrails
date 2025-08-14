477b87c710d532e96c779ec62a242670fff7c6d9
Lucas Sola <lsolagc@hotmail.com>
2025-08-14 20:10:46 -0300

    feat: add common methods for combat

--- modulo03-fundamentos-avancados-ruby/projeto-final/app/models/combatant.rb
+++ modulo03-fundamentos-avancados-ruby/projeto-final/app/models/combatant.rb
@@ -1,3 +1,17 @@
 class Combatant < ApplicationRecord
   delegated_type :combatable, types: %w[ PlayerCharacter ]
+
+  attr_accessor :current_hit_points
+
+  def initialize_for_combat
+    self.current_hit_points = max_hit_points
+  end
+
+  def dead?
+    current_hit_points.nil? || current_hit_points <= 0
+  end
+
+  def initiative
+    dexterity_modifier
+  end
 end
